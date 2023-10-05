b6372458ab52e37e4d69dc7e945c9554d00c595d
Miguel Heleno <miguelheleno@lbl.gov>
2023-10-05 00:17:50 -0700

    update setup.py

--- setup.py
+++ setup.py
@@ -7,11 +7,15 @@ with open('README.md') as f:
 
 setup(
     name='j40model',
-    version='1.0.0',
+    version='1.1.0',
     description='Base simulation and optimization modules for J40 tool',
     long_description=readme,
+    url='https://github.com/LBNLgrid/j40model',
     author='Miguel Heleno',
     author_email='miguelhelenoa@lbl.gov',
     include_package_data=True,
-    packages=['j40_model'],
+    install_requires=['geopandas==0.13',
+                      'numpy==1.25',
+                      'pandas==2.0',
+                      'Pyomo==6.6']
 )
